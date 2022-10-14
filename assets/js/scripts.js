c2235c497018f004952c185d720f9c8294b92d2e
Pavel Uspensky <sitetobe@yandex.ru>
2022-10-14 16:01:39 +0300

    js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,12 +6,12 @@ let navMobile = document.querySelector('.mobile__list.closed'),
     btn = document.querySelector('.mobile__button');
 
 btn.addEventListener('click', function() {
-    if ( navMobile.classList.contains('closed') ) {  
-    navMobile.classList.remove('closed');
-    }
-    else {
-        navMobile.classList.add('closed');   
-    }
+ 
+    navMobile.classList.toggle('closed');
+
+    // else {
+    //     navMobile.classList.add('closed');   
+    // }
 });
 
 }
