69de318608dba4eaa8c0da9d866a4d564237b9af
Pavel Uspensky <sitetobe@yandex.ru>
2022-10-14 14:17:40 +0300

    js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,10 +7,7 @@ let navMobile = document.querySelector('.mobile__list.closed'),
 
 btn.addEventListener('click', function() {
     if ( navMobile.classList.contains('closed') ) {  
-    navMobile.classList.remove('closed');
-    }
-    else {
-        navMobile.classList.add('closed');   
+    navMobile.classList.toggle('closed');
     }
 });
 
@@ -42,62 +39,62 @@ btn.addEventListener('click', function() {
 //     close();
 // }
 
-function modalOpener({ modal, btn, closeBtn, time = 300}) {
+// function modalOpener({ modal, btn, closeBtn, time = 300}) {
 
-    const buttonElem = document.querySelectorAll(btn);
-    const modalElem = document.querySelector(modal);
+//     const buttonElem = document.querySelectorAll(btn);
+//     const modalElem = document.querySelector(modal);
 
-    modalElem.style.cssText = `
-    display: flex;
-    visibility: hidden;
-    opacity: 0;
-    transistion: opacity ${time}ms ease-in-out;
-`;
+//     modalElem.style.cssText = `
+//     display: flex;
+//     visibility: hidden;
+//     opacity: 0;
+//     transistion: opacity ${time}ms ease-in-out;
+// `;
 
-    const closeModal = event => {
-        const target = event.target;
-        if (target === modalElem || 
-            (closeBtn && target.closest(closeBtn))||
-            event.code === 'Escape'
-            ) {
-            modalElem.style.opacity = 0;
+//     const closeModal = event => {
+//         const target = event.target;
+//         if (target === modalElem || 
+//             (closeBtn && target.closest(closeBtn))||
+//             event.code === 'Escape'
+//             ) {
+//             modalElem.style.opacity = 0;
 
-            setTimeout(() => {
-                modalElem.style.visibility = 'hidden';
-            }, time);
-            window,removeEventListener('keydown', closeModal);
-        }
-    };
+//             setTimeout(() => {
+//                 modalElem.style.visibility = 'hidden';
+//             }, time);
+//             window,removeEventListener('keydown', closeModal);
+//         }
+//     };
 
-    const openModal = () => {
-        modalElem.style.visibility = 'visible';
-        modalElem.style.opacity = 1;
-        window.addEventListener('keydown', closeModal)
-    };
+//     const openModal = () => {
+//         modalElem.style.visibility = 'visible';
+//         modalElem.style.opacity = 1;
+//         window.addEventListener('keydown', closeModal)
+//     };
 
-    buttonElem.forEach(btn => {
-        btn.addEventListener('click', openModal);
+//     buttonElem.forEach(btn => {
+//         btn.addEventListener('click', openModal);
 
-    });
+//     });
 
-    modalElem.addEventListener('click', closeModal);
+//     modalElem.addEventListener('click', closeModal);
 
-}
-modalOpener({
-    modal: '.modal1',
-    btn: '.openBtn1',
-    closeBtn: '.modal__close',
-    time: 1000
-});
+// }
+// modalOpener({
+//     modal: '.modal1',
+//     btn: '.openBtn1',
+//     closeBtn: '.modal__close',
+//     time: 1000
+// });
 
-modalOpener({
-    modal: '.modal2',
-    btn: '.openBtn2',
-    closeBtn: '.modal__close'
-});
+// modalOpener({
+//     modal: '.modal2',
+//     btn: '.openBtn2',
+//     closeBtn: '.modal__close'
+// });
 
-modalOpener({
-    modal: '.modal3',
-    btn: '.openBtn3',
-    closeBtn: '.modal__close'
-});
+// modalOpener({
+//     modal: '.modal3',
+//     btn: '.openBtn3',
+//     closeBtn: '.modal__close'
+// });
