78b13b87c188b6b722c82202054a299558cf0ca0
Pavel Uspensky <93841004+site2beru@users.noreply.github.com>
2022-10-18 22:43:54 +0300

    js

--- assets/js/photos.js
+++ assets/js/photos.js
@@ -1,10 +1,13 @@
 {
     imgClick = () => {
         let galImg = document.querySelectorAll('.media__img');
+        // over = document.querySelector('.shadow');
 
         for (let i = 0; i < galImg.length; i++) {
             galImg[i].addEventListener('click', function() {
                 galImg[i].classList.toggle('clicked');
+                // over.innerHTML = `${galImg}`;
+
             });
         }
     }
